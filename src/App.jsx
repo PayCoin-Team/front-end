130f6a346b673b28541f050e6eaa252b59136961
Nanyang7 <skdms9494@naver.com>
2026-01-15 10:35:06 +0900

    차트 하단 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import SplashScreen from './SplashScreen';
 import SignUpScreen from './SignUpScreen'; // 회원가입 컴포넌트 임포트
 import LoginScreen from './LoginScreen';
 import Home from './Home'; // 일반 사용자 홈 화면 임포트
+import ChartScreen from './ChartScreen';  // 차트 컴포넌트 임포트
+import ChargeScreen from './ChargeScreen'; // 충전 컴포넌트 임포트
+import MyPageScreen from './MyPageScreen'; // 마이페이지 컴포넌트 임포트
+import PayScreen from './PayScreen';       // 결제 컴포넌트 임포트
 
 function App() {
   return (
@@ -14,6 +18,13 @@ function App() {
         <Route path="/login" element={<LoginScreen />} />
         {/* 일반 사용자 메인 홈 화면 (추가됨) */}
         <Route path="/home" element={<Home />} />
+        <Route path="/chart" element={<ChartScreen />} />
+        <Route path="/mypage" element={<MyPageScreen />} />
+        <Route path="/charge" element={<ChargeScreen />} />
+        <Route path="/pay" element={<PayScreen />} />
+        
+        {/* 혹시 몰라 추가하는 예외 처리 (잘못된 주소 입력 시 홈으로) */}
+        <Route path="*" element={<Home />} />
         
       </Routes>
     </Router>
